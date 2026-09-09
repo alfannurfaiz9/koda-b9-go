8f15ee07fdfac12f4f2263ecd2e5a0dac613ca33
alfan <alfannurfaizwork@gmail.com>
2026-09-09 05:46:21 -0500

    feat: add new choises for office worker and whiteboard

--- internal/choises.go
+++ internal/choises.go
@@ -13,6 +13,7 @@ func Choises() {
 	fmt.Println("6. Print Person Struct")
 	fmt.Println("7. Payment")
 	fmt.Println("8. Office Worker")
+	fmt.Println("9. Whiteboard")
 	fmt.Println("0. Keluar")
 	fmt.Println("--------------------------------")
 }
